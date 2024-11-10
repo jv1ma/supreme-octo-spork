077fa7aba60e78ddce18c50fa559779bb1152a52
jv1ma <jvivesma@irsl.edu.mx>
2024-11-10 10:42:18 -0600

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -6,7 +6,20 @@ const sex = document.getElementById('sex');
 const ex = document.getElementById('ex');
 const email = document.getElementById('email');
 
+filled = 0;
+
+function checkform () {
+    if (fname.value != "" && age.value != "" && gender.value != "" && sex.value != "" && 
+        email.value != "" && colorblind.value != "") {
+        filled = 1;
+    }
+    else {
+        filled = 0;
+    }
+}
+
 setInterval(function (){
+    checkform();
     if (colorblind.value == "No") {
         document.getElementById('no').style.visibility = "visible";
         document.getElementById('yes').style.visibility = "collapse";
@@ -15,11 +28,15 @@ setInterval(function (){
         document.getElementById('yes').style.visibility = "visible";
         document.getElementById('no').style.visibility = "collapse";
     }
-    document.cookie = `F1=${fname.value}; max-age=31536000;`;
-    document.cookie = `F2=${age.value}; max-age=31536000;`;
-    document.cookie = `F3=${gender.value}; max-age=31536000;`;
-    document.cookie = `F4=${sex.value}; max-age=31536000;`;
-    document.cookie = `F5=${ex.value}; max-age=31536000;`;
-    document.cookie = `F6=${email.value}; max-age=31536000;`;
-    document.cookie = `F7=${colorblind.value}; max-age=31536000;`;
+    if (filled == 0) {
+        document.getElementById('no').style.visibility = "collapse";
+        document.getElementById('yes').style.visibility = "collapse";
+    }
+    document.cookie = `F1=${fname.value}; max-age=31536000; SameSite=Lax;`;
+    document.cookie = `F2=${age.value}; max-age=31536000; SameSite=Lax;`;
+    document.cookie = `F3=${gender.value}; max-age=31536000; SameSite=Lax;`;
+    document.cookie = `F4=${sex.value}; max-age=31536000; SameSite=Lax;`;
+    document.cookie = `F5=${ex.value}; max-age=31536000; SameSite=Lax;`;
+    document.cookie = `F6=${email.value}; max-age=31536000; SameSite=Lax;`;
+    document.cookie = `F7=${colorblind.value}; max-age=31536000; SameSite=Lax;`;
 });
