f53fcaf1ba8eff191974e5de5c65256a6a85629e
jv1ma <jvivesma@irsl.edu.mx>
2024-11-10 10:25:32 -0600

    Add files via upload

--- end.js
+++ end.js
@@ -4,12 +4,14 @@ function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
-data = [getCookie('F1'), getCookie('F2'), getCookie('F3'), getCookie('F4'), getCookie('F5'), getCookie('F6'),  getCookie('F7'),
-    getCookie('A1'), getCookie('A2'), getCookie('A3'), getCookie('B1'), getCookie('B2'), getCookie('B3')];
+function downloadfile() {
+    const link = document.createElement("a");
+    const file = new Blob([getCookie('F1'), getCookie('F2'), getCookie('F3'), getCookie('F4'), getCookie('F5'), getCookie('F6'),  getCookie('F7'),
+    getCookie('A1'), getCookie('A2'), getCookie('A3'), getCookie('B1'), getCookie('B2'), getCookie('B3')], { type: 'text/plain' });
+    link.href = URL.createObjectURL(file);
+    link.download = `${getCookie('testid')}.txt`;
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
 
-const link = document.createElement("a");
-const file = new Blob([data.join('\n')], { type: 'text/plain' });
-link.href = URL.createObjectURL(file);
-link.download = `${getCookie('testid')}.dat`;
-link.click();
-URL.revokeObjectURL(link.href);
+downloadfile();
