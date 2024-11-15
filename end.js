97f22f2839dcd7df5a835450511934128607c0ca
jv1ma <jvivesma@irsl.edu.mx>
2024-11-14 21:40:03 -0600

    Update end.js

--- end.js
+++ end.js
@@ -9,7 +9,7 @@ function downloadfile() {
     const file = new Blob([getCookie('F1'), getCookie('F2'), getCookie('F3'), getCookie('F4'), getCookie('F5'), getCookie('F6'),  getCookie('F7'),
     getCookie('A1'), getCookie('A2'), getCookie('A3'), getCookie('B1'), getCookie('B2'), getCookie('B3')], { type: 'text/plain' });
     link.href = URL.createObjectURL(file);
-    link.download = `${getCookie('F1')}_${getCookie('testid')}.txt`;
+    link.download = `${getCookie('F1')}`+`_`+`${getCookie('testid')}.txt`;
     link.click();
     URL.revokeObjectURL(link.href);
 }
